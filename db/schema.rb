ad0464ec0bca9f54d6be26c498b0c8c063deebdd
Myriie <myribronstein@gmail.com>
2013-07-03 17:10:22 +0200

    Resolution pb pull

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20130703144701) do
 
   create_table "articles", :force => true do |t|
@@ -22,7 +21,6 @@ ActiveRecord::Schema.define(:version => 20130703144701) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
-ActiveRecord::Schema.define(:version => 20130703124031) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
