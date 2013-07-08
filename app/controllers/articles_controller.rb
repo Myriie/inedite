3dca21bccd56e7377b5ac684b4c03d76f40483a8
mauviolay <valentin.gojard@gmail.com>
2013-07-08 10:46:24 +0200

    Suppression dans le controller d'article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,6 @@ class ArticlesController < ApplicationController
   end
   def show
   	@article = Article.find(params[:id])
-  	@clothes = @article.clothes.find(params[:id])
   end
   def index
   	@articles = Article.all
