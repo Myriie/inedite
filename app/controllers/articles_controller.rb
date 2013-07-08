1185ce1cf95efdac95937c644a9f79d6008bcd74
Myriie <myribronstein@gmail.com>
2013-07-08 16:59:55 +0200

    enregistrer un nouvel article depuis le site =D

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
   end
   def show
   	@article = Article.find(params[:id])
@@ -9,5 +10,15 @@ class ArticlesController < ApplicationController
   end
   def cat
   	@type = params[:type]
+  end
+  def create
+    @article  = Article.new(params[:article])
+    if @article.save
+      flash[:success] = "Article created!"
+      redirect_to root_path
+    else
+      flash[:error] = "Ne marche pas"
+      render 'new'
+    end
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -14,5 +14,9 @@
 class Article < ActiveRecord::Base
   attr_accessible :description, :id, :price, :title, :type_art
 
+  validates :title, :presence => true, :uniqueness => true 
+  validates :type_art, :presence => true
+  validates :price, :presence => true
+
   has_many :clothes, dependent: :destroy
 end
