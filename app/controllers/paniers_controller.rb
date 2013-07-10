f00e9e063e6d6d7dab04d4178806f355a41141f9
mauviolay <valentin.gojard@gmail.com>
2013-07-10 12:22:48 +0200

    Correction ajout panier

--- app/controllers/paniers_controller.rb
+++ app/controllers/paniers_controller.rb
@@ -22,34 +22,44 @@ class PaniersController < ApplicationController
 
 
   def add
-    if user_signed_in?
-      if current_user.paniers.empty?
-        panier = Panier.new(user_id: current_user.id, validated: false)
-        panier.save
+    if params[:quantity].nil?
+      redirect_to '/articles'
+    else
+
+      if params[:quantity].to_i==0
+        redirect_to '/articles'
       else
-        if current_user.paniers.last.validated?
-          panier = Panier.new(user_id: current_user.id, validated: false)
-          panier.save
-        
+
+        if user_signed_in?
+          if current_user.paniers.empty?
+            panier = Panier.new(user_id: current_user.id, validated: false)
+            panier.save
+          else
+            if current_user.paniers.last.validated?
+              panier = Panier.new(user_id: current_user.id, validated: false)
+              panier.save
+            
+            else
+              panier = current_user.paniers.last
+            end
+          end
+
+          list_res = panier.reservations.all
+          index = list_res.index {|e| e.article_id == params[:article_id].to_i}
+
+          if index.nil?
+            res = Reservation.new(article_id: params[:article_id],panier_id: panier.id, quantity: params[:quantity])
+          else
+            res = list_res.at(index)
+            res.quantity+=params[:quantity].to_i
+          end
+          res.save
+          redirect_to '/panier'
         else
-          panier = current_user.paniers.last
+          flash[:error] = "Veuillez vous connecter pour avoir un panier"
+          redirect_to '/users/sign_in'
         end
       end
-
-      list_res = panier.reservations.all
-      index = list_res.index {|e| e.article_id == params[:article_id].to_i}
-
-      if index.nil?
-        res = Reservation.new(article_id: params[:article_id],panier_id: panier.id, quantity: params[:quantity])
-      else
-        res = list_res.at(index)
-        res.quantity+=params[:quantity].to_i
-      end
-      res.save
-      redirect_to '/panier'
-    else
-      flash[:error] = "Veuillez vous connecter pour avoir un panier"
-      redirect_to '/users/sign_in'
     end
   end
 
