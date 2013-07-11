a90cfa503e9d7e2c2674f23fa7ede608faeb9fd1
mauviolay <valentin.gojard@gmail.com>
2013-07-11 11:31:48 +0200

    Correction create Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,26 @@
 class ArticlesController < ApplicationController
+
+
   def new
     @article = Article.new
   end
+
+
   def show
   	@article = Article.find(params[:id])
   end
+
+
   def index
   	@articles = Article.all
   end
+
+
   def cat
   	@type = params[:type]
   end
+
+
   def create
     @article  = Article.new(params[:article])
     if @article.save
@@ -21,11 +31,16 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
+
   def edit
     @article = Article.find(params[:id])
   end
+  
 
   def gestionArticle
     @articles = Article.all.sort_by{|a| a.title}
   end
+
+
 end
