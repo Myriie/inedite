72a88cd65c59cfb20c273ed146397000e6ead12c
mauviolay <valentin.gojard@gmail.com>
2013-07-11 11:16:19 +0200

    Factorisation controller panier

--- app/controllers/paniers_controller.rb
+++ app/controllers/paniers_controller.rb
@@ -1,94 +1,34 @@
 class PaniersController < ApplicationController
 
+   before_filter :login, :only => [:show,:destroy,:add,:edit,:flush]
+   before_filter :set_panier, :only => [:destroy,:show, :add,:edit,:flush]
+   before_filter :set_article_id, :only => [:add,:destroy,:edit]
+   before_filter :set_quantity, :only => [:add, :edit]
+   before_filter :panier_vide, :only => [:destroy,:edit,:flush]
+
+
   def show
-  	if user_signed_in?
-      if current_user.paniers.empty?
-        @panier = Panier.new(user_id: current_user.id, validated: false)
-        @panier.save
-      else
-    		if current_user.paniers.last.validated? 
-      		@panier = Panier.new(user_id: current_user.id, validated: false)
-      		@panier.save
-     	 	
-     	 	else
-     	 		@panier = current_user.paniers.last
-     	 	end
-       end
-  	else
-  		flash[:error] = "Veuillez vous connecter pour avoir un panier"
-  		redirect_to '/users/sign_in'
-  	end
   end
 
 
   def add
-    if params[:quantity].nil?
-      redirect_to '/articles'
+    list_res = @panier.reservations.all
+    index = list_res.index {|e| e.article_id == @article.id}
+
+    if index.nil?
+      res = Reservation.new(article_id: @article.id,panier_id: @panier.id, quantity: @quantity)
     else
-
-      if params[:quantity].to_i==0
-        redirect_to '/articles'
-      else
-
-        if user_signed_in?
-          if current_user.paniers.empty?
-            panier = Panier.new(user_id: current_user.id, validated: false)
-            panier.save
-          else
-            if current_user.paniers.last.validated?
-              panier = Panier.new(user_id: current_user.id, validated: false)
-              panier.save
-            
-            else
-              panier = current_user.paniers.last
-            end
-          end
-
-          list_res = panier.reservations.all
-          index = list_res.index {|e| e.article_id == params[:article_id].to_i}
-
-          if index.nil?
-            res = Reservation.new(article_id: params[:article_id],panier_id: panier.id, quantity: params[:quantity])
-          else
-            res = list_res.at(index)
-            res.quantity+=params[:quantity].to_i
-          end
-          res.save
-          redirect_to '/panier'
-        else
-          flash[:error] = "Veuillez vous connecter pour avoir un panier"
-          redirect_to '/users/sign_in'
-        end
-      end
+      res = list_res.at(index)
+      res.quantity+=@quantity
     end
+    res.save
+    redirect_to '/panier'
   end
 
+
   def destroy
-    unless user_signed_in?
-      flash[:error] = "Veuillez vous connecter pour avoir un panier"
-      redirect_to '/users/sign_in'
-    end
-    if params[:article_id].to_i == 0
-      flash[:error] = "Vous ne pouvez pas tapez ce que vous voulez dans l'url"
-      redirect_to root_path
-    end
-     unless !params[:article_id].nil?
-      flash[:error] = "Vous ne pouvez pas tapez ce que vous voulez dans l'url"
-      redirect_to root_path
-    end
-    unless !current_user.paniers.empty?
-      flash[:error] = "Vous n'avez pas de panier"
-      redirect_to root_path
-    end
-    unless !current_user.paniers.last.validated?
-      flash[:error] = "Vous n'avez pas de panier"
-      redirect_to root_path
-    end
-
-    panier = current_user.paniers.last
-
-    list_res = panier.reservations.all
-    index = list_res.index {|e| e.article_id == params[:article_id].to_i}
+    list_res = @panier.reservations.all
+    index = list_res.index {|e| e.article_id == @article.id}
 
     if index.nil?
       redirect_to '/panier'
@@ -99,75 +39,92 @@ class PaniersController < ApplicationController
     end
   end
 
+
   def edit
-    unless user_signed_in?
-      flash[:error] = "Veuillez vous connecter pour avoir un panier"
-      redirect_to '/users/sign_in'
-    end
-    if params[:article_id].to_i == 0
-      flash[:error] = "Vous ne pouvez pas tapez ce que vous voulez dans l'url"
-      redirect_to root_path
-    end
-     unless !params[:article_id].nil?
-      flash[:error] = "Vous ne pouvez pas tapez ce que vous voulez dans l'url"
-      redirect_to root_path
-    end
-    if params[:quantity].to_i ==0
-      flash[:error] = "Vous ne pouvez pas tapez ce que vous voulez dans l'url"
-      redirect_to root_path
-    end
-    unless !params[:quantity].nil?
-      flash[:error] = "Vous ne pouvez pas tapez ce que vous voulez dans l'url"
-      redirect_to root_path
-    end
-    unless !current_user.paniers.empty?
-      flash[:error] = "Vous n'avez pas de panier"
-      redirect_to root_path
-    end
-    unless !current_user.paniers.last.validated?
-      flash[:error] = "Vous n'avez pas de panier"
-      redirect_to root_path
-    end
-
-    panier = current_user.paniers.last
-
-    list_res = panier.reservations.all
+    list_res = @panier.reservations.all
     index = list_res.index {|e| e.article_id == params[:article_id].to_i}
 
     if index.nil?
       redirect_to '/panier'
     else
       res = list_res.at(index)
-      res.quantity = params[:quantity].to_i
+      res.quantity = @quantity
       res.save
       redirect_to '/panier'
     end
   end
 
-  def flush
-    unless user_signed_in?
-      flash[:error] = "Veuillez vous connecter pour avoir un panier"
-      redirect_to '/users/sign_in'
-    end
-        unless !current_user.paniers.empty?
-      flash[:error] = "Vous n'avez pas de panier"
-      redirect_to root_path
-    end
-    unless !current_user.paniers.last.validated?
-      flash[:error] = "Vous n'avez pas de panier"
-      redirect_to root_path
-    end
 
-    panier = current_user.paniers.last
-    panier.reservations.each do |r|
+  def flush
+    @panier.reservations.each do |r|
       r.destroy.save
     end
     redirect_to '/panier'
-
   end
 
+
+
+protected
+
+    def login
+      unless user_signed_in?
+        flash[:error] = "Veuillez vous connecter pour avoir un panier"
+        redirect_to '/users/sign_in'
+      end
+    end
+
+
+    def set_panier
+      if current_user.paniers.empty?
+        @panier = Panier.new(user_id: current_user.id, validated: false)
+        @panier.save
+      else
+        if current_user.paniers.last.validated? 
+          @panier = Panier.new(user_id: current_user.id, validated: false)
+          @panier.save
+        else
+          @panier = current_user.paniers.last
+        end
+      end
+    end
+
+
+    def set_article_id
+      if params[:article_id].to_i == 0
+        flash[:error] = "Vous ne pouvez pas tapez ce que vous voulez dans l'url"
+        redirect_to root_path
+      end
+      unless !params[:article_id].nil?
+        flash[:error] = "Vous ne pouvez pas tapez ce que vous voulez dans l'url"
+        redirect_to root_path
+      end
+      @article = Article.find(params[:article_id].to_i)
+    end
+
+
+    def set_quantity
+      if params[:quantity].to_i ==0
+        flash[:error] = "Vous ne pouvez pas tapez ce que vous voulez dans l'url"
+        redirect_to root_path
+      end
+      unless !params[:quantity].nil?
+        flash[:error] = "Vous ne pouvez pas tapez ce que vous voulez dans l'url"
+        redirect_to root_path
+      end
+      @quantity = params[:quantity].to_i
+    end
+
+
+    def panier_vide
+        unless !current_user.paniers.empty?
+        flash[:error] = "Vous n'avez pas de panier"
+        redirect_to root_path
+      end
+      unless !current_user.paniers.last.validated?
+        flash[:error] = "Vous n'avez pas de panier"
+        redirect_to root_path
+      end
+    end
+
+
 end
-
-
-
-
